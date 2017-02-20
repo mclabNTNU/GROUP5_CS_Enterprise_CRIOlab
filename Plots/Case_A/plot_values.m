533e29debb19904cf39665ce885e1639138775e2
mclabNTNU <mclabNTNU@gmail.com>
2017-02-20 12:26:52 +0100

    With plots and logging

---
+++ Plots/Case_A/plot_values.m
@@ -0,0 +1,6 @@
+load('psi)
+f1 = figure
+hold on
+grid on
+subplot(2, 1, 1);
+plot(u_hat)
--- simulink source/init.m
+++ simulink source/init.m
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 clear
 eta_0 = [0 0 0];
 nu_0 =[0 0 0];
-Par.tau_max = [1.03 0 0];
 Par.K_psi_init = 0.3;
 %% outside workspace
 Par.Workspace.origin = [0, 0];
@@ -26,51 +24,13 @@ Par.Freeze.threshold = 100;%3.0;
 Par.Step_size = 0.01;
 Par.Sample_time = 0.01;
 Par.Sample_rate = 1/Par.Sample_time;
+Par.Noise.Power = 1e-5;
 
 
-Par.Thrust_lim = [1.03 2.5 0.98]';
-
-%% Lowpass filter
-Par.Lowpass.omega.c_u = 100;
-Par.Lowpass.omega.c_v = Par.Lowpass.omega.c_u;
-Par.Lowpass.omega.c_psi = Par.Lowpass.omega.c_u;
-
-=======
-clear
-eta_0 = [0 0 0];
-nu_0 =[0 0 0];
-Par.tau_max = [1.03 0 0];
-Par.K_psi_init = 0.3;
-%% outside workspace
-Par.Workspace.origin = [0, 0]';
-Par.Workspace.threshold = 2;
-Par.Workspace.x_max_limit = 10;
-Par.Workspace.x_min_limit = -Par.Workspace.x_max_limit;
-Par.Workspace.y_max_limit = Par.Workspace.x_max_limit;
-Par.Workspace.y_min_limit = -Par.Workspace.x_max_limit;
-
-Par.Workspace.x_min = Par.Workspace.x_min_limit + Par.Workspace.threshold;
-Par.Workspace.x_max = Par.Workspace.x_max_limit - Par.Workspace.threshold;
-Par.Workspace.y_min = Par.Workspace.y_min_limit + Par.Workspace.threshold;
-Par.Workspace.y_max = Par.Workspace.y_max_limit - Par.Workspace.threshold;
-
-%% noise and dropout
-Par.SensNoiseEnabled = 1;
-Par.Freeze.Enable = 0;
-Par.Freeze.length = 2;
-Par.Freeze.threshold = 100;%3.0;
-
-Par.Step_size = 0.01;
-Par.Sample_time = 0.01;
-Par.Sample_rate = 1/Par.Sample_time;
-Par.Noise.Power = 1e-4;
-
-
-Par.Thrust_lim = [1.03 2.5 0]; %[1.03 2.5 0.98]';
+Par.Thrust_lim = [1.03 2.5 0];%[1.03 2.5 0.98]';
 
 %% Lowpass filter
 Par.Lowpass.omega.c_u = 100;
 Par.Lowpass.omega.c_v = Par.Lowpass.omega.c_u;
 Par.Lowpass.omega.c_psi = Par.Lowpass.omega.c_u;
 
->>>>>>> origin/master
